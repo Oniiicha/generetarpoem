49ff87a031bbd9446e67c6a2628905b0f2c7fa67
Onii Chan <122252034+Oniiicha@users.noreply.github.com>
2024-01-12 16:31:41 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,13 +13,27 @@ let poems = [
             Это яркий Новый год <br>
             В двери к нам стучится. <br>
         </p>`
-        // bg: ``
+    },
+    {
+        poemText:
+        `<p>
+            В двери Новый год стучится, <br>
+            Дед Мороз к нам в гости мчится, <br>
+            В небе праздничный салют, <br>
+            И часы двенадцать бьют. <br>
+        </p>
+        <p>
+            Огоньки сверкают ярко. <br>
+            Свечи, шарики, подарки. <br>
+            Скоро сказка в дом войдет, <br>
+            Скоро будет Новый год! <br>
+        </p>`
     }
 ]
 
 let wrapper = document.querySelector('.wrapper');
 let btn = document.getElementById('btn');
-let contantText = document.querySelector('.poem');
+let poem = document.querySelector('.poem');
 
 btn.addEventListener('click', function() {
     poem.innerHTML = '';
